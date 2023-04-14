6c082aa71af9dc7c3501bb4540be36df47066573
Adam Makuchowski <maku971@gmail.com>
2023-04-14 20:13:41 +0200

    created main banner

---
+++ src/components/Slogan/SloganStyled.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components'
+
+export const SloganContainer = styled.div`
+  height: 200px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export const SloganContentWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  border-bottom: solid 1px #C0C0C0;
+`
+
+export const WecomeWrapper = styled.div`
+  color: #708238;
+  font-size: 30px;
+  margin: 5px;
+`
+
+export const VineyardWrapper = styled.div`
+  font-size: 30px;
+  margin-bottom: 30px;
+  font-weight: bold;
+`
