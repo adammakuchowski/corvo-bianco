0fb0ea15704451b451cf21a1e1ae25e0c4fc321b
Adam Makuchowski <maku971@gmail.com>
2023-04-17 20:17:10 +0200

    created second and rhigt about content

--- src/components/About/AboutContent/AboutContent.tsx
+++ src/components/About/AboutContent/AboutContent.tsx
@@ -3,51 +3,95 @@ import {greatVibes, playfairDisplay, sacramento, tangerine} from '@/pages'
 import {
   AboutContentContainer,
   ContentWarpper,
-  FirstContentAutograph,
-  FirstContentDescriptionContainer,
-  FirstContentDescriptionWrapper,
-  FirstContentHeaderWrapper,
-  FirstContentImgContainer,
-  FirstContentImgWrapper,
+  ContentAutograph,
+  ContentDescriptionContainer,
+  ContentDescriptionWrapper,
+  LeftContentHeaderWrapper,
+  CircleContentImgContainer,
+  CircleContentImgWrapper,
+  SecondContentImgContainer,
+  SecondContentImgWrapper,
+  RightContentHeaderWrapper,
 } from './AboutContentStyled'
 
-const img = '/images/firstContentImg.jpg'
+const imgs = [
+  '/images/leftContentImg.jpg',
+  '/images/middleContentImg.jpg',
+  '/images/rightContentImg.jpg',
+]
 
 const AboutContent = (): JSX.Element => {
 
   return (
     <AboutContentContainer>
       <ContentWarpper>
-        <FirstContentHeaderWrapper>
+        <LeftContentHeaderWrapper>
           <div className={playfairDisplay.className}>Toussaint</div>
           <div className={playfairDisplay.className}>2015</div>
           <div className={greatVibes.className}>Wineyard</div>
-        </FirstContentHeaderWrapper>
+        </LeftContentHeaderWrapper>
 
-        <FirstContentImgContainer>
-          <FirstContentImgWrapper>
+        <CircleContentImgContainer>
+          <CircleContentImgWrapper>
             <Image
-              src={img}
+              src={imgs[0]}
               alt=''
               fill
               style={{objectFit: 'cover'}}
             />
-          </FirstContentImgWrapper>
-        </FirstContentImgContainer>
+          </CircleContentImgWrapper>
+        </CircleContentImgContainer>
 
-        <FirstContentDescriptionContainer>
-          <FirstContentDescriptionWrapper className={tangerine.className}>
+        <ContentDescriptionContainer>
+          <ContentDescriptionWrapper className={tangerine.className}>
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat velit ipsa suscipit cupiditate provident autem optio nobis ea tenetur vero delectus, neque natus id aut rerum hic expedita incidunt iusto.
-          </FirstContentDescriptionWrapper>
-          <FirstContentAutograph className={sacramento.className}>
+          </ContentDescriptionWrapper>
+          <ContentAutograph className={sacramento.className}>
             Geralt
-          </FirstContentAutograph>
-        </FirstContentDescriptionContainer>
-
+          </ContentAutograph>
+        </ContentDescriptionContainer>
       </ContentWarpper>
 
-      <ContentWarpper></ContentWarpper>
-      <ContentWarpper></ContentWarpper>
+      <ContentWarpper>
+        <SecondContentImgContainer>
+          <SecondContentImgWrapper>
+            <Image
+              src={imgs[1]}
+              alt=''
+              fill
+              style={{objectFit: 'cover'}}
+            />
+          </SecondContentImgWrapper>
+        </SecondContentImgContainer>
+      </ContentWarpper>
+
+      <ContentWarpper>
+        <RightContentHeaderWrapper>
+          <div className={playfairDisplay.className}>Anna Henrietta</div>
+          <div className={playfairDisplay.className}>Creative Director</div>
+          <div></div>
+        </RightContentHeaderWrapper>
+
+        <CircleContentImgContainer>
+          <CircleContentImgWrapper>
+            <Image
+              src={imgs[2]}
+              alt=''
+              fill
+              style={{objectFit: 'cover'}}
+            />
+          </CircleContentImgWrapper>
+        </CircleContentImgContainer>
+
+        <ContentDescriptionContainer>
+          <ContentDescriptionWrapper className={tangerine.className}>
+            <q>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form.</q>
+          </ContentDescriptionWrapper>
+
+          <div> - - - </div>
+
+        </ContentDescriptionContainer>
+      </ContentWarpper>
     </AboutContentContainer>
   )
 }
