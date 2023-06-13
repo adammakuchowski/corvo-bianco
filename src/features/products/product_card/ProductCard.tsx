f1d7e41ccbf4228cbdfaaf28fabcea7b11303885
Adam Makuchowski <maku971@gmail.com>
2023-06-13 18:12:59 +0200

    update active ProductCard action

--- src/features/products/product_card/ProductCard.tsx
+++ src/features/products/product_card/ProductCard.tsx
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 import Image from 'next/image'
 import {Product} from '@/types/types'
 import ProductCardAction from './product_card_action/ProductCardAction'
@@ -10,16 +10,30 @@ import {
 } from './ProductCardStyled'
 
 interface ProductCardProps {
+  index: number;
   product: Product;
+  setActiceCardIndex: (value: number) => void;
+  activeCard: boolean;
 }
 
-const ProductCard = ({product}: ProductCardProps): JSX.Element => {
-  const [isActive, setIsActive] = useState<boolean>(false)
+const ProductCard = ({index, product, setActiceCardIndex, activeCard = false}: ProductCardProps): JSX.Element => {
+  const [isActive, setIsActive] = useState<boolean>(activeCard)
   const {img} = product
+
+  useEffect(() => {
+    setIsActive(activeCard)
+  }, [activeCard])
+
+  const onCardActive = () => {
+    setIsActive(!isActive)
+    if (!isActive) {
+      setActiceCardIndex(index)
+    }
+  }
 
   return (
     <ProductCardContainer>
-      <ImgContainer onClick={() => setIsActive(!isActive)} active={isActive}>
+      <ImgContainer onClick={onCardActive} active={isActive}>
         <ImgWrapper>
           <Image
             src={img}
