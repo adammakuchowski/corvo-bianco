564870c748d557d0a9c0493452aece147213f3f0
Adam Makuchowski <maku971@gmail.com>
2023-06-20 20:06:36 +0200

    update addToCart reducer - productSilce

--- src/features/products/product_cart/product_cart_entry_counter/ProductCartEntryCounter.tsx
+++ src/features/products/product_cart/product_cart_entry_counter/ProductCartEntryCounter.tsx
@@ -1,5 +1,6 @@
-import {AiOutlineMinus, AiOutlinePlus} from 'react-icons/ai'
+import {useEffect, useState} from 'react'
 import {useDispatch} from 'react-redux'
+import {AiOutlineMinus, AiOutlinePlus} from 'react-icons/ai'
 import IconButton from '@/components/buttons/icon_button/IconButton'
 import {ProductCart} from '@/types/types'
 import {ProductCartEntryCounterContainer, ProductQuantityWrapper} from './ProductCartEntryCounterStyled'
@@ -12,27 +13,41 @@ interface ProductCartEntryCounterProps {
 }
 
 const ProductCartEntryCounter = ({productCart}: ProductCartEntryCounterProps): JSX.Element => {
+  const [isDecrementButtonDisabled, setIsDecrementButtonDisabled] = useState<boolean>(false)
   const dispatch = useDispatch()
 
   const {product: {id}, quantity} = productCart
 
+  useEffect(() => {
+    setIsDecrementButtonDisabled(quantity === 1)
+  }, [quantity])
+
   const incrementQuantityProduct = () => {
-    dispatch(updateQuantityProductCart({id, actionOperator: 'increment'}))
+    dispatch(updateQuantityProductCart({id, actionOperator: 'increment', value: 1}))
   }
 
   const decrementQuantityProduct = () => {
-    if (quantity > 1) {
-      dispatch(updateQuantityProductCart({id, actionOperator: 'decrement'}))
-    }
+    if (isDecrementButtonDisabled) return
+    
+    dispatch(updateQuantityProductCart({id, actionOperator: 'decrement', value: 1}))
   }
 
   return (
     <ProductCartEntryCounterContainer>
-      <IconButton iconComponent={<AiOutlineMinus />} fontSize={fontSize} iconAction={decrementQuantityProduct} />
+      <IconButton
+        iconComponent={<AiOutlineMinus />}
+        fontSize={fontSize}
+        iconAction={decrementQuantityProduct}
+        disabled={isDecrementButtonDisabled}
+      />
       <ProductQuantityWrapper>
         {quantity}
       </ProductQuantityWrapper>
-      <IconButton iconComponent={<AiOutlinePlus />} fontSize={fontSize} iconAction={incrementQuantityProduct} />
+      <IconButton
+        iconComponent={<AiOutlinePlus />}
+        fontSize={fontSize}
+        iconAction={incrementQuantityProduct}
+      />
     </ProductCartEntryCounterContainer>
   )
 }
