601d78b5594bb1c00d6f1f5218a9b509e299b99a
Adam Makuchowski <maku971@gmail.com>
2023-07-12 19:14:47 +0200

    init Contact page

---
+++ src/pages/contact.tsx
@@ -0,0 +1,11 @@
+import {ContactContainer} from './styles/contactStyled'
+
+const Contact = (): JSX.Element => {
+  
+  return (
+    <ContactContainer>
+    </ContactContainer>
+  )
+}
+
+export default Contact
