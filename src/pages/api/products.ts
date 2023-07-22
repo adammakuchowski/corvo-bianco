0ca58f78fff020d17e6d5cf2a1eff1c74b6af58d
Adam Makuchowski <maku971@gmail.com>
2023-07-22 14:40:26 +0200

    init product post method

--- src/pages/api/products.ts
+++ src/pages/api/products.ts
@@ -1,4 +1,4 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type {NextApiRequest, NextApiResponse} from 'next'
 import {HttpMethod} from './constants'
 import dbConnect from '../../../db/utils/mongo'
 import Product from '../../../db/models/Product'
@@ -12,15 +12,21 @@ export default async function handler(
   res: NextApiResponse<Data>
 ) {
   const {method} = req
-  await dbConnect()
+  dbConnect()
 
   if (req.method === HttpMethod.GET) {
-    
-  } 
-  
-  if (req.method === HttpMethod.POST) {
-    
-  } 
 
-  res.status(200).json({ name: 'John Doe' })
+  }
+
+  if (req.method === HttpMethod.POST) {
+    try {
+      const product = await Product.create(req.body)
+
+      res.status(201).json(product as any)
+    } catch (err) {
+      res.status(500).json(err as Data)
+    }
+  }
+
+  res.status(200).json({name: 'John Doe'})
 }
