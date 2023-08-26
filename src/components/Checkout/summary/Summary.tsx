1fab5290a8a6919da8c82cf6bac44a4f64652d42
Adam Makuchowski <maku971@gmail.com>
2023-08-26 10:32:03 +0200

    init behaviour create order result

--- src/components/Checkout/summary/Summary.tsx
+++ src/components/Checkout/summary/Summary.tsx
@@ -1,13 +1,83 @@
 import {useContext, useEffect, useState} from 'react'
+import {useSelector} from 'react-redux'
+import {BsCheckCircle} from 'react-icons/bs'
+import {VscError} from 'react-icons/vsc'
 import CircularProgress from '@mui/material/CircularProgress'
+import {AppState} from '@/app/store'
+import {cinzel} from '@/fonts/fonts'
 import CheckoutContext from '@/context/CheckoutContext'
 import CheckoutForm from '@/features/checkout/checkout_form/CheckoutForm'
-import {SummaryContainer} from './SummaryStyled'
+import {
+  CreateOrderContainer,
+  IconWrapper,
+  OrderCreatorContainer,
+  SummaryContainer,
+  SummaryContentContainer,
+  ResultOrderTextWrapper,
+} from './SummaryStyled'
 import CheckoutContents from '../checkout_contents/CheckoutContents'
+
+interface OrderProgressCreatorProps {
+  orderCreateStatus: 'loading' | 'succeeded' | 'failed';
+}
+
+const OrderCreatorFulfilled = () => {
+  return (
+    <OrderCreatorContainer>
+      <IconWrapper style={{color: '#8ea648'}}>
+        <BsCheckCircle />
+      </IconWrapper>
+      <ResultOrderTextWrapper className={cinzel.className}>
+        Thank you for placing your order in our winiari, we will try to make delivery as soon as possible.
+      </ResultOrderTextWrapper>
+    </OrderCreatorContainer>
+  )
+}
+
+const OrderCreatorError = () => {
+  return (
+      <OrderCreatorContainer>
+      <IconWrapper style={{color: '#e32323'}}>
+          <VscError />
+        </IconWrapper>
+        <ResultOrderTextWrapper className={cinzel.className}>
+          There was a problem while creating your order, please contact support or try again later.
+        </ResultOrderTextWrapper>
+      </OrderCreatorContainer>
+    )
+}
+
+const OrderProgressCreator = ({orderCreateStatus}: OrderProgressCreatorProps) => {
+  const [orderCratorResultComponent, setOrderCratorResultComponent] = useState<JSX.Element>()
+
+  useEffect(() => {
+    const orderCratorResult = orderCreateStatus === 'succeeded' ? OrderCreatorFulfilled : OrderCreatorError
+    setOrderCratorResultComponent(orderCratorResult)
+  }, [orderCreateStatus])
+
+  return (
+    <CreateOrderContainer>
+      {(orderCreateStatus !== 'loading') ?
+        <>{orderCratorResultComponent}</> :
+        <CircularProgress sx={{color: '#8ea648'}} size={50} />
+      }
+    </CreateOrderContainer>
+  )
+}
+
+const SummaryContent = () => {
+  return (
+    <SummaryContentContainer>
+      <CheckoutForm />
+      <CheckoutContents />
+    </SummaryContentContainer>
+  )
+}
 
 const Summary = () => {
   const {className} = useContext(CheckoutContext)
   const [delayClassName, setDelayClassName] = useState<string>('')
+  const orderCreateStatus = useSelector((state: AppState) => state.checkout.orderCreateStatus)
 
   useEffect(() => {
     setTimeout(() => {
@@ -15,11 +85,15 @@ const Summary = () => {
     }, 300)
   }, [className])
 
+  useEffect(() => {
+    // TODO: implement resetting of order status and shopping cart status 
+  }, [orderCreateStatus])
+
   return (
     <SummaryContainer className={delayClassName}>
-      {/* <CircularProgress sx={{color: '#8ea648'}} /> */}
-      <CheckoutForm/>
-      <CheckoutContents />
+      {orderCreateStatus === 'idle' ?
+        <SummaryContent /> :
+        <OrderProgressCreator orderCreateStatus={orderCreateStatus} />}
     </SummaryContainer>
   )
 }
