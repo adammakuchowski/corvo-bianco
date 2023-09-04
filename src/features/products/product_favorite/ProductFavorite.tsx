c6c00d03bfc23aa1d6e1868bedaacbab704e8046
Adam Makuchowski <maku971@gmail.com>
2023-09-04 20:20:54 +0200

    init FavoriteProducts SidePanel

--- src/features/products/product_favorite/ProductFavorite.tsx
+++ src/features/products/product_favorite/ProductFavorite.tsx
@@ -1,11 +1,45 @@
+import {useEffect, useState} from 'react'
+import {useSelector} from 'react-redux'
+import SidePanel from '@/components/common/side_panel/SidePanel'
 import {ProductFavoriteContainer} from './ProductFavoriteStyled'
 
-const ProductFavorite = (): JSX.Element => {
+interface ProductFavoriteProps {
+  favoriteIsOpen: boolean;
+  setFavoriteIsOpen: (value: boolean) => void;
+}
+
+const ProductFavorite = ({favoriteIsOpen, setFavoriteIsOpen}: ProductFavoriteProps): JSX.Element | null => {
+  const favoriteProducts = useSelector(getFavoriteProducts)
+  const [isTrashDisabled, setIsTrashDisabled] = useState<boolean>(false)
+  const [favoriteTotal, setFavoriteTotal] = useState<number>(0)
+
+  useEffect(() => {
+    favoriteProducts.length ? setIsTrashDisabled(false) : setIsTrashDisabled(true)
+
+  }, [favoriteProducts])
+
+  const onCloseModal = () => setFavoriteIsOpen(false)
+
+  const onClearFavorite = () => {
+    dispatch(clearFavorite())
+    localStorage.setItem('favoriteProducts ', JSON.stringify([]))
+  }
+
+  if (!favoriteIsOpen) return null
 
   return (
+    <SidePanel
+      sidePanelIsOpen={favoriteIsOpen}
+      setIsSidePanelOpen={setFavoriteIsOpen}
+      onClear={onClearFavorite}
+      isTrashDisabled={isTrashDisabled}
+      onCloseModal={onCloseModal}
+      headerName='FAVORITE'
+    >
     <ProductFavoriteContainer>
 
     </ProductFavoriteContainer>
+    </SidePanel>
   )
 }
 
